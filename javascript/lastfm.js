0e61a5939fdd532cb8cbe7ec238ff299be59cc11
Giovani Finazzi <53719063+GiovaniFZ@users.noreply.github.com>
2024-11-30 18:30:43 -0300

    Change some styles and refactor code (#1)
    * style: fix cursor for blue text on initial page
    
    * refactor: Change some ifs to objects and separate some elements in consts
    
    * style: Change background color, add animation, set red color on anchors, and set bold text for title
    
    * style: Change font to Barlow

--- javascript/lastfm.js
+++ javascript/lastfm.js
@@ -1,79 +1,86 @@
 const apiKey = localStorage.getItem("apiKey");
 console.log("reading from localStorage...");
 
+
+async function fetchPlayData() {
+  const username = document.getElementById("userInput").value;
+  document.getElementById('lastStatus').style.display = "";
+  document.getElementById('lastFirstUi').style.display = "none";
+  const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json`);
+  const data = await response.json();
+  const track = data.recenttracks.track[0];
+  console.log("making last.fm api request...");
+  console.log("track object:", track);
+  const isPlaying = track["@attr"] && track["@attr"].nowplaying;
+
+  const artistName = document.getElementById('artistName')
+  const albumName = document.getElementById('albumName')
+  const trackTitle = document.getElementById('trackTitle');
+  const trackMbid = document.getElementById('trackMbid');
+  const trackLink = document.getElementById('trackLink');
+  const artistMbid = document.getElementById('artistMbid');
+  const albumMbid = document.getElementById('albumMbid');
+  const albumArtDesc = document.getElementById('albumArtDesc');
+  const albumArt = document.getElementById('albumArt');
+
+  if (isPlaying) {
+    trackTitle.innerText = track.name;
+    track.artist["#text"] ? artistName.innerText = `${track.artist["#text"]}` : artistName.innerText = "Unknown (N/A)";
+    if (track.mbid) {
+      trackMbid.innerText = `${track.mbid}`;
+      trackMbid.href = `https://musicbrainz.org/recording/${track.mbid}`;
+    } else {
+      trackMbid.innerText = "Unknown (N/A)";
+    }
+    if (track.url) {
+      trackLink.innerText = `${track.url}`;
+      trackLink.href = `${track.url}`;
+    } else {
+      trackLink.innerText = "Unknown (N/A)";
+    }
+    track.album["#text"] ? albumName.innerText = `${track.album["#text"]}` : albumName.innerText = "Unknown (N/A)";
+    if (track.image[3]["#text"]) {
+      const img = new Image();
+      img.src = track.image[3]["#text"];
+      img.onload = function () {
+        albumArtDesc.innerText = `Download album art (${this.width + 'x' + this.height})`;
+      }
+      albumArt.src = track.image[3]["#text"];
+      albumArtDesc.href = track.image[3]["#text"];
+      albumArtDesc.download = "AlbumArt.jpg";
+    } else {
+      albumArtDesc.innerText = "No album art available";
+      albumArtDesc.href = "";
+      albumArt.src = "https://lastfm.freetls.fastly.net/i/u/4128a6eb29f94943c9d206c08e625904.jpg";
+    }
+    if (track.album.mbid) {
+      albumMbid.innerText = `${track.album.mbid}`;
+      albumMbid.href = `https://musicbrainz.org/release/${track.album.mbid}`;
+    } else {
+      albumMbid.innerText = "Unknown (N/A)";
+    }
+    if (track.artist.mbid) {
+      artistMbid.innerText = `${track.artist.mbid}`;
+      artistMbid.href = `https://musicbrainz.org/artist/${track.artist.mbid}`;
+    } else {
+      artistMbid.innerText = "Unknown (N/A)";
+    }
+  } else {
+    trackTitle.innerText = "None";
+    artistName.innerText = "None";
+    albumName.innerText = "None";
+    trackLink.innerText = "None";
+    trackMbid.innerText = "None";
+    artistMbid.innerText = "None";
+    albumMbid.innerText = "None";
+    albumArtDesc.innerText = "Nothing is playing";
+    albumArt.src = "https://lastfm.freetls.fastly.net/i/u/4128a6eb29f94943c9d206c08e625904.jpg";
+  }
+}
+
 async function fetchNowPlaying() {
   try {
-    const username = document.getElementById("userInput").value;
-    document.getElementById('lastStatus').style.display = "";
-    document.getElementById('lastFirstUi').style.display = "none";
-    const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json`);
-    const data = await response.json();
-    const track = data.recenttracks.track[0];
-    console.log("making last.fm api request...");
-    console.log("track object:", track);
-    const isPlaying = track["@attr"] && track["@attr"].nowplaying;
-
-    if (isPlaying) {
-      document.getElementById('trackTitle').innerText = track.name;
-      if (track.artist["#text"]) {
-        document.getElementById('artistName').innerText = `${track.artist["#text"]}`;
-      } else {
-        document.getElementById('artistName').innerText = "Unknown (N/A)";
-      }
-      if (track.mbid) {
-        document.getElementById('trackMbid').innerText = `${track.mbid}`;
-        document.getElementById('trackMbid').href = `https://musicbrainz.org/recording/${track.mbid}`;
-      } else {
-        document.getElementById('trackMbid').innerText = "Unknown (N/A)";
-      }
-      if (track.url) {
-        document.getElementById('trackLink').innerText = `${track.url}`;
-        document.getElementById('trackLink').href = `${track.url}`;
-      } else {
-        document.getElementById('trackLink').innerText = "Unknown (N/A)";
-      }
-      if (track.album["#text"]) {
-        document.getElementById('albumName').innerText = `${track.album["#text"]}`;
-      } else {
-        document.getElementById('albumName').innerText = "Unknown (N/A)";
-      }
-      if (track.image[3]["#text"]) {
-        const img = new Image();        
-        img.src = track.image[3]["#text"];
-        img.onload = function() {
-          document.getElementById('albumArtDesc').innerText = `Download album art (${this.width + 'x' + this.height})`;
-        }
-        document.getElementById('albumArt').src = track.image[3]["#text"];
-        document.getElementById('albumArtDesc').href = track.image[3]["#text"];
-        document.getElementById('albumArtDesc').download = "AlbumArt.jpg";
-      } else {
-        document.getElementById('albumArtDesc').innerText = "No album art available";
-        document.getElementById('albumArtDesc').href = "";
-        document.getElementById('albumArt').src = "https://lastfm.freetls.fastly.net/i/u/4128a6eb29f94943c9d206c08e625904.jpg";
-      }
-      if (track.album.mbid) {
-        document.getElementById('albumMbid').innerText = `${track.album.mbid}`;
-        document.getElementById('albumMbid').href = `https://musicbrainz.org/release/${track.album.mbid}`;
-      } else {
-        document.getElementById('albumMbid').innerText = "Unknown (N/A)";
-      }
-      if (track.artist.mbid) {
-        document.getElementById('artistMbid').innerText = `${track.artist.mbid}`;
-        document.getElementById('artistMbid').href = `https://musicbrainz.org/artist/${track.artist.mbid}`;
-      } else {
-        document.getElementById('artistMbid').innerText = "Unknown (N/A)";
-      }
-    } else {
-      document.getElementById('trackTitle').innerText = "None";
-      document.getElementById('artistName').innerText = "None";
-      document.getElementById('albumName').innerText = "None";
-      document.getElementById('trackLink').innerText = "None";
-      document.getElementById('trackMbid').innerText = "None";
-      document.getElementById('artistMbid').innerText = "None";
-      document.getElementById('albumMbid').innerText = "None";
-      document.getElementById('albumArtDesc').innerText = "Nothing is playing";
-      document.getElementById('albumArt').src = "https://lastfm.freetls.fastly.net/i/u/4128a6eb29f94943c9d206c08e625904.jpg";
-    }
+    await fetchPlayData();
   } catch (error) {
     console.error('error when searching data from last.fm:', error);
   }
